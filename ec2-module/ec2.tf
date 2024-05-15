4f5ce801e397295da22c2340ecceeeca226b8d13
krisp09 <krishnascm21@gmail.com>
2024-05-15 20:45:08 +0530

    ec2 with Module

--- ec2-module/ec2.tf
+++ ec2-module/ec2.tf
@@ -1,4 +1,3 @@
-/*
 resource "aws_instance" "app_server" {
   ami           = "ami-0ff8a91507f77f867"
   instance_type = var.instance_type
@@ -6,11 +5,4 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "TestServerInstance"
   }
-}
-*/
-module "ec2-instance" {
-  source  = "./ec2-module/"
-  //version = "5.6.1"
-  instance_type = var.instance_type
-  ami = var.ami
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0ff8a91507f77f867"
+}
